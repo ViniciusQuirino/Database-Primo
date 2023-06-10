4c1c3dfa506ab2acb4e3dc8be1364bb681cbd7d1
ViniciusQuirino <viniciusquirino13@hotmail.com>
2023-06-10 10:40:13 -0300

    project finished

--- src/controllers/tempo/controller.controller.ts
+++ src/controllers/tempo/controller.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 
 const controllerController = async (req: Request, res: Response) => {
-  return res.status(201).json({ oi: "oi" });
+  return res.status(200).json({ oi: "oi" });
 };
 export default controllerController;
