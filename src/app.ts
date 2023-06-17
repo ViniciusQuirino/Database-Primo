3bc20a4055d5e30c99e344b53ac529f881bd7649
ViniciusQuirino <viniciusquirino13@hotmail.com>
2023-06-17 09:59:18 -0300

    project finished

--- src/app.ts
+++ src/app.ts
@@ -25,7 +25,7 @@ app.use("/produtos", produtosRoutes);
 
 app.use(handleErrorMiddleware);
 
-const job = new CronJob("0 0 */2 * *", async () => {
+const job = new CronJob("0 2 * * 1", async () => {
   const pedidosRepositorio: Repository<Pedidos> =
     AppDataSource.getRepository(Pedidos);
 
